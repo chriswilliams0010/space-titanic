bf43cb0f8dcefeaa2130d39002b33c42769ccbe3
Chris Williams <chris.williams0010@gmail.com>
2022-04-26 18:27:11 -0400

    encoded home planet feature

--- st_eda.py
+++ st_eda.py
@@ -1,10 +1,37 @@
 import pandas as pd
+import numpy as np
+from sklearn import preprocessing
 
-# read in the training set
+# -read in the training set
 df = pd.read_csv('train.csv')
 
-# check out missingness and dtypes
+# -check out missingness and dtypes
 df.isnull().sum()
 df.info(null_counts=False)
 df.describe()
+
+# -begin data wrangling
+# loop for splitting PassengerId into two and saving each split in a list
+group = []  # group list initialized
+ind = []  # individual list initialized
+for pid in df['PassengerId']:
+    group.append(pid.split("_")[0])
+df['group'] = group  # add group column with first split
+
+# prepare a dict with columns that need to change dtypes
+change_dtype = {'group': 'int'}
+
+# start a list of columns to drop
+drop = ['PassengerId']
+
+# add a column of value counts for group
+df['alone'] = df['group'].map(df['group'].value_counts())
+# change to binary
+df['alone'] = np.where(df['alone'] == 1, 1, 0)
+
+# change HomePlanet to factor
+le_hp = preprocessing.LabelEncoder()
+le_hp.fit(df['HomePlanet'])
+df['home_planet'] = le_hp.transform(df['HomePlanet'])
+drop.append('HomePlanet')
 
